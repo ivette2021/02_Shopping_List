5dca9e0f09d1162f042a464490c40c965e9977b6
iconsWork <iconstanzav1@gmail.com>
2024-06-03 17:13:46 -0400

    -mostrando nuestros items con ion-list -mostrando nuestros items con ion-list -opciones de items con ion-item-sliding -eliminar item -reordenar elementos con ion-reorder-group -menu -eliminar todos los items

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,68 @@
+import { AlertController, MenuController } from '@ionic/angular';
 import { Component } from '@angular/core';
+import { ShoppingItemsService } from '../shopping-items.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  constructor(
+    public shoppingList: ShoppingItemsService,
+    private alertController: AlertController,
+    private menuController: MenuController
+  ) {}
 
-  constructor() {}
+  async removeItem(item: string) {
+    const alert = await this.alertController.create({
+      header: 'Confirmacion',
+      message: '¿Estas seguro de borrar el item?',
+      buttons: [
+        {
+          text: 'Si',
+          handler: () => {
+            this.shoppingList.removeItem(item);
+          },
+        },
+        {
+          text: 'No',
+          handler: () => {
+            alert.dismiss();
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
+  onRenderItems($event) {
+    console.log($event);
 
+    const item = this.shoppingList.items.splice($event.detail.from, 1)[0];
+    this.shoppingList.items.splice($event.detail.to, 0, item);
+    $event.detail.complete();
+    console.log(this.shoppingList.items);
+  }
+  async removeAll() {
+    const alert = await this.alertController.create({
+      header: 'Confirmacion',
+      message: '¿Estas seguro de borrar todos los item?',
+      buttons: [
+        {
+          text: 'Si',
+          handler: () => {
+            this.shoppingList.removeAllItems();
+            this.menuController.close();
+          },
+        },
+        {
+          text: 'No',
+          handler: () => {
+            alert.dismiss();
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
 }
