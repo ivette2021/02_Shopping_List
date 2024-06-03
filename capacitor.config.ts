a1a8a4f448893d5f76cbe78a14cb629c33fbc894
iconsWork <iconstanzav1@gmail.com>
2024-06-03 10:59:34 -0400

    manejo de tabs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: '02_shopping_list',
-  webDir: 'www'
+  appId: 'com.ivette.Shopping_List',
+  appName: 'Shopping List',
+  webDir: 'www',
 };
 
 export default config;
---
+++ src/app/shopping-items.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ShoppingItemsService {
+  public items: string[];
+  public isEmpty: boolean;
+
+  constructor() {
+    this.items = [];
+    this.isEmpty = true;
+  }
+  addItem(item: string) {
+    this.items.push(item);
+    this.isEmpty = false;
+  }
+  removeItem(item: string) {
+    let index = this.items.findIndex(
+      (it) => it.toUpperCase().trim() === item.toUpperCase().trim()
+    );
+    if (index != -1) {
+      this.items.splice(index, 1);
+      if (this.items.length == 0) {
+        this.isEmpty = true;
+      }
+    }
+  }
+  removeAllItems() {
+    this.items = [];
+    this.isEmpty = true;
+  }
+  existsitem(item: string) {
+    const itemFound = this.items.find(
+      (it) => it.toUpperCase().trim() === item.toUpperCase().trim()
+    );
+    return itemFound;
+  }
+}
